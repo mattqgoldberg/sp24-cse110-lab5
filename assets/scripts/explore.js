0b2cf8519754b04cb6b9d0f508488c94e7ba75d1
Matt Goldberg <matt.q.goldberg@gmail.com>
2024-05-06 16:56:12 -0700

    Finished text to speech functionality

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,39 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
+let synth = window.speechSynthesis;
+synth.addEventListener('voiceschanged', populateVoices);
 
 function init() {
-  // TODO
+  let talkButton = document.querySelector('button');
+  talkButton.addEventListener("click", playSpeech);
+
+  synth.addEventListener("")
+}
+
+function populateVoices() {
+  let selector = document.getElementById('voice-select');
+  let voices = synth.getVoices();
+  console.log(voices);
+  for (let i = 0; i < voices.length; i++) {
+    let voice = document.createElement('option');
+    voice.innerHTML = voices[i].name;
+    voice.value = i;
+    selector.appendChild(voice);
+  }
+}
+
+function playSpeech() {
+  let text = document.querySelector('textarea').value;
+  console.log(text);
+  let utterance = new SpeechSynthesisUtterance();
+
+  utterance.voice = synth.getVoices()[document.getElementById('voice-select').value];
+  utterance.text = text;
+  let mouth = document.querySelector('img');
+  utterance.addEventListener('end', () => {
+    mouth.src = 'assets/images/smiling.png';
+  })
+  mouth.src = 'assets/images/smiling-open.png';
+  synth.speak(utterance); 
 }
