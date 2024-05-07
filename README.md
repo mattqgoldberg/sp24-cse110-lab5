527597779e843290df65845c2fcfa759c9087633
Matt Goldberg <matt.q.goldberg@gmail.com>
2024-05-06 17:49:29 -0700

    Created Unit Tests

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Matthew Goldberg
 
 [Expose](https://mattqgoldberg.github.io/sp24-cse110-lab5/expose)
+[Expose](https://mattqgoldberg.github.io/sp24-cse110-lab5/explore)
+
+1. Using a unit test to test a send message feature would be difficult. It would require you to have 2 instances of the client running in order to test both sending and receiving functionality. It is difficult to test how this send message function would interact with the server and the other client so a unit test is likely not good. You could test if the function is sending the correct format of message but not how it interacts with the other parts.
+
+2. Something like this is very simple and effective to use a unit test for. You can very easily check the input and output of the function you are testing to see if the 80 character limit is working.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,92 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+
+//PHONE NUMBER TESTS
+test('too short phone number', () => {
+  expect(isPhoneNumber('1234')).toBe(false);
+});
+
+test('letters', () => {
+  expect(isPhoneNumber('my phone number')).toBe(false);
+});
+
+test('use parenthesis', () => {
+  expect(isPhoneNumber('(324)-432-1369')).toBe(true);
+});
+
+test('10 digit number', () => {
+  expect(isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+// EMAIL TESTS
+
+test('no @ symbol', () => {
+  expect(isEmail('johndoegmail.com')).toBe(false);
+});
+
+test('no .', () => {
+  expect(isEmail('johndoe@gmailcom')).toBe(false);
+});
+
+test('gmail', () => {
+  expect(isEmail('johndoe@gmail.com')).toBe(true);
+});
+
+test('ucsd email', () => {
+  expect(isEmail('johndoe@ucsd.edu')).toBe(true);
+});
+
+// PASSWORD TESTS
+
+test('too short password', () => {
+  expect(isStrongPassword('a')).toBe(false);
+});
+
+test('whitespace', () => {
+  expect(isStrongPassword('pass word')).toBe(false);
+});
+
+test('strong password 1', () => {
+  expect(isStrongPassword('abd0d3dA0')).toBe(true);
+});
+
+test('normal password', () => {
+  expect(isStrongPassword('dogCollar2004')).toBe(true);
+});
+
+// DATE TESTS
+
+test('year only', () => {
+  expect(isDate('2004')).toBe(false);
+});
+
+test('2004 shortened to 04', () => {
+  expect(isDate('12/25/04')).toBe(false);
+});
+
+test('today', () => {
+  expect(isDate('05/06/2024')).toBe(true);
+});
+
+test('today shortened', () => {
+  expect(isDate('5/6/2024')).toBe(true);
+});
+
+// HEX COLOR TESTS
+
+test('4 characters', () => {
+  expect(isHexColor('3FFA')).toBe(false);
+});
+
+test('uses letters G-Z', () => {
+  expect(isHexColor('A3G')).toBe(false);
+});
+
+test('white', () => {
+  expect(isHexColor('FFFFFF')).toBe(true);
+});
+
+test('white shortened', () => {
+  expect(isHexColor('FFF')).toBe(true);
+});
